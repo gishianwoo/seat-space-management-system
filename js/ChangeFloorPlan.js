5bc9477ba47ea638cf6e8ccc19ed5c87faaa338c
landuochongyuchen <134469092+landuochongyuchen@users.noreply.github.com>
2023-06-03 02:12:06 +0800

    Add files via upload

---
+++ js/ChangeFloorPlan.js
@@ -0,0 +1,20 @@
+var Floor = 1
+function Floor(Floor){
+    switch(Floor){
+        case 1:
+            document.getElementById("floor").src = "../images/1F.jpg";
+            break
+        case 2:
+            document.getElementById("floor").src = "../images/2F.jpg";
+            break
+        case 3:
+            document.getElementById("floor").src = "../images/3F.jpg";
+            break
+        case 4:
+            document.getElementById("floor").src = "../images/4F.jpg";
+            break
+        case 5:
+            document.getElementById("floor").src = "../images/5F.jpg";
+            break
+    }
+}
